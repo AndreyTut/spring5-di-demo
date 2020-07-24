02aee6a56f319e017840329e9139130d299b6931
Andrey <andreww@ukr.net>
2020-07-24 20:42:39 +0300

    my impl application.properties

--- src/main/java/guru/springframework/config/PropConfig.java
+++ src/main/java/guru/springframework/config/PropConfig.java
@@ -2,24 +2,12 @@ package guru.springframework.config;
 
 import guru.springframework.examplebeans.FakeDataSource;
 import guru.springframework.examplebeans.FakeJmsBroker;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
-import org.springframework.context.annotation.PropertySources;
-import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
-import org.springframework.core.env.Environment;
 
 @Configuration
-@PropertySources({
-        @PropertySource("classpath:datasource.properties"),
-        @PropertySource("classpath:jmsbroker.properties")
-})
 public class PropConfig {
-
-    @Autowired
-    Environment environment;
 
     @Value("${db.username}")
     private String username;
@@ -40,15 +28,9 @@ public class PropConfig {
     private String jmsUrl;
 
     @Bean
-    public static PropertySourcesPlaceholderConfigurer placeholder() {
-        return new PropertySourcesPlaceholderConfigurer();
-    }
-
-    @Bean
     public FakeDataSource dataSource() {
         FakeDataSource dataSource = new FakeDataSource();
- //       dataSource.setUserName(username);
-        dataSource.setUserName(environment.getProperty("USERNAME"));
+        dataSource.setUserName(username);
         dataSource.setPsw(psw);
         dataSource.setUrl(db_url);
         return dataSource;
